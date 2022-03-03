0530591a80380822a5cfd260f3cca3fe1d3dbcff
andres Londoño <andresmedellin215@gmail.com>
2022-03-02 21:07:54 -0500

    Inicio del programa

--- AbarrotesElRopero/Program.cs
+++ AbarrotesElRopero/Program.cs
@@ -7,13 +7,19 @@ namespace AbarrotesElRopero
         static void Main(string[] args)
         {
             Menu menu = new();//SE INSTANCIA EL MENU PARA SACAR CUALQUIER MENU DEL OBJETO
-            var nombreEmpresa = "EL ROPERO";
+            var nombreEmpresa = "ABARROTES EL ROPERO";
+             int ;
 
+            Console.WriteLine($"******************* {nombreEmpresa} ******************");
+            Console.WriteLine("\nMODULOS DE LA EMPRESA :");
+            menu.MenuModulos();//SACA EL MENU DE TODOS LOS MODULOS EXISTENTES
+            Console.WriteLine("\ningres la opcion que desea escoger :");
+            
+            
+            do
+            {
 
-            Console.WriteLine($"*******************{nombreEmpresa} ******************");
-            Console.WriteLine("MODULOS DE LA EMPRESA :");
-
-
+            } while (true);
 
 
 
